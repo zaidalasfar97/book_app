48a28f8a2870132ea45c12a244b213fcba5b7230
zaidalasfar97 <z.alasfar@hotmail.com>
2021-03-01 01:49:52 -0800

    finish

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
 'use strict'
 const express = require("express");
 const { get } = require("superagent");
-const su = require("superagent");
-// const path=require('path')
+const su = require("superagent")
 const app = express();
-require("dotenv").config();
+require("dotenv").config()
 app.use(express.static('./public'));
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 
-const PORT = process.env.PORT || 2000;
+const Port = process.env.PORT || 2000
 app.use(express.urlencoded({ extended: true }));
-app.listen(PORT, () => {
+app.listen(Port, () => {
+    console.log(`listening on ${Port}`)
 
 })
-app.get('/', homePage);
-app.post('/searches', getData);
-app.get('/searches/new', newSearch);
-app.get('/searches/show', showData);
+app.get('/', homePage)
+app.post('/searches', getData)
+app.get('/searches/new', newSearch)
+// app.get('/searches/show', showData)
 let bookValues = [];
+// function showData(req, res) {
 
-
-function showData(req, res) {
-
-    res.render("pages/searches/show", { Data: bookValues })
-}
+//     res.render("pages/searches/show", { Data: bookValues })
+// }
 
 
 function getData(req, res) {
@@ -37,34 +35,36 @@ function getData(req, res) {
     //selecting from form
     let sel = req.body.TorA
     // console.log(y); this.url=data.image || https://i.imgur.com/J5LVHEL.jpg
-    let url = `https://www.googleapis.com/books/v1/volumes?q=${search}+${sel}`;
+    // let trigger;
+    // if(tri)
+    let url = `https://www.googleapis.com/books/v1/volumes?q=+${sel}:${search}`;
     su.get(url)
         .then((result) => {
-            bookValues = []
+            let boolImage;
             // res.send(result.body.items[4].volumeInfo);
 
             let itemsARR = result.body.items.map(function (elem, idx) {
                 //    r.push(elem.volumeInfo.title)
-                // console.log(elem.volumeInfo.title);
-                if (idx < 9) {
-                    let bookResult = new getBookData(elem)
-                    bookValues.push(bookResult);
+
+                if (typeof elem.volumeInfo.imageLinks == "undefined") {
+                    // console.log("false");
+                    boolImage = false;
+                } else {
+                    boolImage = true
+                    // console.log("true");
                 }
+
+                return new Book(elem, boolImage)
+
+
 
 
             })
 
+            // console.log(itemsARR.imageURL);
+            res.render("pages/searches/show", { Data: itemsARR })
 
 
-
-            function getBookData(data) {
-                this.imageURL = data.volumeInfo.imageLinks.thumbnail || "https://i.imgur.com/J5LVHEL.jpg"
-                this.bookTitle = data.volumeInfo.title || "There Is No Title"
-                this.authorName = data.volumeInfo.authors || "There Is No Author"
-                this.description = data.volumeInfo.description || "There Is No Description"
-
-            }
-            res.redirect('/searches/show')
         })
 
 
@@ -72,11 +72,27 @@ function getData(req, res) {
 function newSearch(req, res) {
 
 
-    res.render("pages/searches/new");
+    res.render("pages/searches/new")
 }
 
 
 function homePage(req, res) {
-    res.render("pages/index");
+    res.render("pages/index")
+
+}
+
+function Book(data, image) {
+    if (image == false) {
+        // console.log("dd");
+        this.imageURL = "https://i.imgur.com/J5LVHEL.jpg"
+
+    } else if (image) {
+        // console.log("LL");
+        this.imageURL = data.volumeInfo.imageLinks.smallThumbnail
+    }
+    this.bookTitle = data.volumeInfo.title || "There Is No Title"
+    this.authorName = data.volumeInfo.authors || "There Is No Author"
+
+    this.description = data.volumeInfo.description || "There Is No Description"
 
 }
