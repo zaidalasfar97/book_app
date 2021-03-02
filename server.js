d00ba5e02811d8fd56fbd81b658eeea0e2272205
zaidalasfar97 <z.alasfar@hotmail.com>
2021-03-02 02:23:48 -0800

    lab13e

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const pg = require("pg");
 const methodOverride = require('method-override');
 
 require("dotenv").config()
-const client = new pg.Client(process.env.DATABASE_URL);
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+// const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
 app.use(express.static('./public'));
 
